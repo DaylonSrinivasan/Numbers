3123d058d59ebe847d96bf858fbfc295a4b7f29a
Daylon Srinivasan <daylon.srinivasan@gmail.com>
2016-04-07 20:06:21 -0700

    Multiplayer set up

--- app/src/main/java/com/daylon/numbers/TitlePage.java
+++ app/src/main/java/com/daylon/numbers/TitlePage.java
@@ -1,5 +1,6 @@
 package com.daylon.numbers;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.design.widget.FloatingActionButton;
@@ -7,11 +8,15 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 public class TitlePage extends AppCompatActivity {
     ImageView title;
     CountDownTimer timer;
+    ImageButton singleplayer;
+    ImageButton versus;
+    ImageButton leaderboard;
 
 
 
@@ -23,6 +28,27 @@ public class TitlePage extends AppCompatActivity {
         setSupportActionBar(toolbar);
         title = (ImageView) findViewById(R.id.title);
         animateTitle();
+
+        //buttons!
+        singleplayer = (ImageButton) findViewById(R.id.singleplayer);
+        versus = (ImageButton) findViewById(R.id.versus);
+        leaderboard = (ImageButton) findViewById(R.id.leaderboard);
+
+        singleplayer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(TitlePage.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+        versus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(TitlePage.this, MultiplayerGame.class);
+                startActivity(i);
+            }
+        });
 
     }
     public void animateTitle(){
